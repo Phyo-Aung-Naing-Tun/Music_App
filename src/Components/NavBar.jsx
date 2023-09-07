58e1debbf8b93928220c7518a8752090fb169324
Phyo-Aung-Naing-Tun <phyotun24@gmail.com>
2023-09-07 10:02:30 +0630

    adding navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { FiPlus } from "react-icons/fi";
+
+const NavBar = () => {
+  return (
+    <div className=" d-flex justify-content-evenly align-items-center py-3 shadow">
+      <h1 className=" fst-italic  fw-bold fs-4 text-primary ">My Music</h1>
+      <div className=" d-flex gap-2">
+        <form>
+          <div className="search-input  input-group">
+            <input
+              placeholder="Search songs"
+              className="  form-control "
+              type="text"
+            />
+            <div
+              style={{ width: "40px" }}
+              className=" fw-bolder btn btn-sm border btn-ouline-primary text-primary fs-2  d-flex justify-content-center align-items-center "
+            >
+              <BiSearchAlt2 />
+            </div>
+          </div>
+        </form>
+
+        <button className=" d-flex btn align-items-center btn-primary text-uppercase fw-semibold ">
+          <FiPlus className=" fs-4  me-2" />
+          songs
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/Paths/Paths.jsx
+++ src/Paths/Paths.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MusicHome from "../Pages/MusicHome";
+import NavBar from "../Components/NavBar";
 
 const Paths = () => {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<MusicHome />} />
       </Routes>
